68972cb6815c3aa1a77fb6b213eb94947e767733
Vinícius do Carmo Melício <vinicmelicio@gmail.com>
2023-02-13 17:20:44 -0300

    site rodando com react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav/Nav.js';
+import Home from './components/home/Home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const About = () => <h1>About</h1>;
+const Contact = () => <h1>Contact</h1>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
